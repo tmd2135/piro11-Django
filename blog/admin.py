14b8e55b10147d343c13510d83a730e1f588ad30
tmd2135 <tmd2135@naver.com>
2019-07-20 03:52:59 +0900

    blog.models.Post 모델에 status 필드를 추가하고, 이를 처리하는 Custom Admin Action 구현

--- blog/admin.py
+++ blog/admin.py
@@ -6,15 +6,27 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id','title','content_size','created_at','updated_at']
+    list_display = ['id','title','content_size', 'status','created_at','updated_at']
+
+    actions = ['make_draft','make_published']
 
     def content_size(self,post):
         return mark_safe('<strong>{}</strong>글자'.format(len(post.content)))
     content_size.short_description = '글자수'
 
+    def make_draft(self,request,queryset):
+        update_count = queryset.update(status='p')
+        self.message_user(request,'{}건의 포스팅을 Draft상태로 변경'.format(update_count))
+    make_draft.short_description = '지정 포스팅을 Draft상태로 변경'
+
+    def make_published(self,request,queryset):
+        update_count = queryset.update(status='p')
+        self.message_user(request,'{}건의 포스팅을 Published상태로 변경'.format(update_count))
+    make_published.short_description = '지정 포스팅을 Published상태로 변경'
 #admin에 Post를 등록
 #한번만 등록
 # admin.site.register(Post)
 #기본유저를 바꾸고싶을때 해지하고 등록할때 사용
 # admin.site.unregister(Post)
+
 
--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,10 @@ def lnglat_validator(value):
 
 # Create your models here.
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('d','Draft'),
+        ('p','Published')
+    )
     author = models.CharField(max_length=20)
     title = models.CharField(max_length=100, verbose_name='제목')    # 길이 제한 o
     content = models.TextField(verbose_name='내용')                # 길이 제한 x
@@ -14,5 +18,6 @@ class Post(models.Model):
     lnglat = models.CharField(max_length=50,blank=True,
                 help_text='위도/경도 포맷으로 입력',
                 validators=[lnglat_validator])
+    status= models.CharField(max_length=1,choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)    #auto_now_add : 최초 저장할시 일시 저장
     updated_at = models.DateTimeField(auto_now=True)        #auto_now : 갱신이 될 때 마다 저장
