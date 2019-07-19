e8d02c018aa04ccd641a0d50d301c05d66afd1e6
tmd2135 <tmd2135@naver.com>
2019-07-20 01:46:57 +0900

    blog.models.Post 모데 생성/마이그레이션/admin등록

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,17 @@
+import re
 from django.db import models
+from django.forms import ValidationError
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*),value'):
+        raise ValidationError('Invalid LngLat Type')
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name='제목')    # 길이 제한 o
     content = models.TextField(verbose_name='내용')                # 길이 제한 x
+    tags= models.CharField(max_length=100,blank=True)
+    lnglat = models.CharField(max_length=50,blank=True,
+                help_text='위도/경도 포맷으로 입력',
+                validators=[lnglat_validator])
     created_at = models.DateTimeField(auto_now_add=True)    #auto_now_add : 최초 저장할시 일시 저장
     updated_at = models.DateTimeField(auto_now=True)        #auto_now : 갱신이 될 때 마다 저장
