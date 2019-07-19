e504782ad93f11c7f0eda2cc9f1f2b9990f30b4a
tmd2135 <tmd2135@naver.com>
2019-07-20 00:35:08 +0900

    함수 기반 뷰 (FBV) 의 4가지 패턴

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'blog',
     'dojo',
+    'myapp',
 ]
 
 MIDDLEWARE = [
--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,15 @@
 from django.urls import re_path
 from . import views
+from . import views_cbv
 
 urlpatterns = [
     re_path(r'^list1/$',views.post_list1),
     re_path(r'^list2/$',views.post_list2),
     re_path(r'^list3/$', views.post_list3),
     # re_path(r'^excel/$', views.excel_download()),
+
+    re_path(r'^cbv/list1/$',views_cbv.post_list1),
+    re_path(r'^cbv/list2/$',views_cbv.post_list2),
+    # re_path(r'^cbv/list3/$', views_cbv.post_list3),
+    # re_path(r'^excel/$', views_cbv.excel_download()),
 ]
