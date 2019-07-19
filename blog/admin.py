21d64bba7173e944942d5e5300fcd9a00889f41f
tmd2135 <tmd2135@naver.com>
2019-07-20 01:35:02 +0900

    blog.models.Post 모데 생성/마이그레이션/admin등록

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+#admin에 Post를 등록
+admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name='제목')    # 길이 제한 o
+    content = models.TextField(verbose_name='내용')                # 길이 제한 x
+    created_at = models.DateTimeField(auto_now_add=True)    #auto_now_add : 최초 저장할시 일시 저장
+    updated_at = models.DateTimeField(auto_now=True)        #auto_now : 갱신이 될 때 마다 저장
