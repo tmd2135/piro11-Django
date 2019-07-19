83e29dd17950f236d57e7773e291d92c4bd25e2a
tmd2135 <tmd2135@naver.com>
2019-07-19 22:17:06 +0900

    dojo.views.hello : 한글 문자열 매칭

--- dojo/urls.py
+++ dojo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     re_path(r'^sum/(?P<numbers>[\d/]+)/$',views.mysum),
+    re_path(r'^hello/(?P<name>[ㄱ-힣]+)/((?P<age>\d+)$',views.hello),
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -6,3 +6,6 @@ def mysum(request,numbers):
     # numbers = "1233/12/2131242/123124215/12312/31/3
     result = sum(map(lambda s : int(s or 0),numbers.split("/")))
     return HttpResponse(result)
+
+def hello(request,name,age):
+    return HttpResponse('안녕하세요.{}.{}살이시네요.',format(name,age))
