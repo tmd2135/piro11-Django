f30cb3f487b52ed9cb66768029263d669c59964b
tmd2135 <tmd2135@naver.com>
2019-07-23 00:49:52 +0900

    account 수정

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)      #나쁜 케이스임 !!
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
