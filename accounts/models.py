463c9a162618d74f93b6885be2e51ab304957f72
tmd2135 <tmd2135@naver.com>
2019-07-23 00:58:37 +0900

    setting.AUTH_USER_MODEL 올바른 지정

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,7 @@ from django.conf import settings
 
 
 class Profile(models.Model):
+    #OnetoOne 일시에는 setting.AUTH_USER_MODEL로 쓰는게 좋다
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)      #나쁜 케이스임 !!
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
